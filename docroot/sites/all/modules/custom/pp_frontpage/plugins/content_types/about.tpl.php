e141f2a8367accf2561e50c8b20dcf202f9416b9
anpolimus <anatolii@hnb.com.ua>
2016-06-03 00:01:18 +0300

    Updated about widget

--- docroot/sites/all/modules/custom/pp_frontpage/plugins/content_types/about.tpl.php
+++ docroot/sites/all/modules/custom/pp_frontpage/plugins/content_types/about.tpl.php
@@ -1,1 +1,5 @@
-<h1>TEST of THEME</h1>
+<div id="logo-large"><img src="/sites/all/themes/custom/ppdorg_bootstrap/logo-large.png"></div>
+<h3>
+  <?php print t('PPDORG is a project that scans Drupal.org contributions and prepare reports about contributions, grouped by country parameter.');?> <br>
+  <?php print t('Current version investigate Drupal communities of the eastern Europe');?>
+</h3>
