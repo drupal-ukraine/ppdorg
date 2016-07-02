24d4fa31bbd9c87561647f54eb161d35f61be790
anpolimus <anatolii@hnb.com.ua>
2016-07-02 17:36:13 +0300

    Rebuilt community_quality with right hook_theme approach

---
+++ docroot/sites/all/modules/custom/pp_frontpage/plugins/content_types/templates/community_quality.tpl.php
@@ -0,0 +1,16 @@
+<div class="bs-callout bs-callout-primary">
+  <ul>
+    <li>
+      <?php print t('Total count');?>: <strong><?php print $total;?></strong>
+    </li>
+    <li>
+      <?php print t('Active committers');?> : <strong><?php print $active_committers;?> (<?php print $active_committers_p;?> %)</strong>
+    </li>
+    <li>
+      <?php print t('Active commenters');?> : <strong><?php print $active_commenters;?> (<?php print $active_commenters_p;?> %)</strong>
+    </li>
+    <li>
+      <?php print t('Individual assoc members');?> : <strong><?php print $assoc_individual_members;?> (<?php print $assoc_individual_members_p;?> %)</strong>
+    </li>
+  </ul>
+</div>
